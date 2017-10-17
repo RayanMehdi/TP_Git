a7651b0e614ca0d3982dedbce54448c865a51f61
iem <iem@iutabgdin202-02.univ-lyon1.fr>
2017-10-17 16:39:54 +0200

    Creation de l'activity Recherche avec un listView pour l'affichage des notes

--- app/src/main/java/com/example/iem/tp_git/MainActivity.java
+++ app/src/main/java/com/example/iem/tp_git/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.iem.tp_git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTest = (Button)findViewById(R.id.buttonTest);
+
+        
+
+
+
     }
 }
