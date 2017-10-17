daefa5ecc28f42e304477603e0b30b6e926f9676
adam <adam.mebarki@etu.univ-lyon1.fr>
2017-10-17 16:21:10 +0200

    Add button of the main page Adam o/

--- app/src/main/java/com/example/iem/tp_git/MainActivity.java
+++ app/src/main/java/com/example/iem/tp_git/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.iem.tp_git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public Button createButton;
+    public Button listButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createButton = (Button) findViewById(R.id.createButton);
+        listButton = (Button) findViewById(R.id.listButton);
+        createButton.setOnClickListener(this);
+        listButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+            if(view.getId() == createButton.getId())
+            {
+
+            }
+            if(view.getId() == listButton.getId())
+            {
+
+
+            }
     }
 }
